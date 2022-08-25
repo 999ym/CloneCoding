737b07039869cc5402885c7cec87336fb35bef2a
9yeonmi <yeonmi47@gmail.com>
2022-08-25 21:40:54 +0900

    jpa

---
+++ example1/src/main/java/com/example/example1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.example1.repository;
+
+import com.example.example1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
